c7b4d6bb77739d6f022f29b24940c21c0c77c915
Liam McClelland <mcclelland-109773@3aaa.ac.uk>
2018-02-16 13:10:38 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -58,7 +58,7 @@ def input_to_index(input)
 end
 
 def move(board, position, character = "X")
-  if valid_move(board, position)
+  if valid_move?(board, position)
     board[position] = character
   else
     return
